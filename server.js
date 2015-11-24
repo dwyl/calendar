5cc4511f9d755ccbf48dfaa0af68eb70db9d2c24
Nelson <contact.nelsonic@gmail.com>
2015-11-24 16:46:04 +0000

    everything you need to get started with the project

---
+++ server.js
@@ -0,0 +1,67 @@
+require('env2')('.env');
+var assert = require('assert');
+var Hapi   = require('hapi'); // require the hapi module
+var server = new Hapi.Server();
+
+server.connection({
+	host: 'localhost',
+	port: Number(process.env.PORT) // defined by environment variable or .env file
+});
+
+var scopes = [
+  'https://www.googleapis.com/auth/plus.profile.emails.read',
+  'https://www.googleapis.com/auth/calendar.readonly'
+]
+
+var opts = {
+  REDIRECT_URL: '/googleauth',  // must match google app redirect URI
+  handler: require('./lib/google_oauth_handler.js'), // your handler
+  scope: scopes // profile
+};
+
+var hapi_auth_google = require('hapi-auth-google');
+
+var plugins = [
+	{ register: hapi_auth_google, options:opts },
+	require('hapi-auth-jwt2')
+];
+server.register(plugins, function (err) {
+  // handle the error if the plugin failed to load:
+  assert(!err, "FAILED TO LOAD PLUGIN!!! :-("); // fatal error
+	// see: http://hapijs.com/api#serverauthschemename-scheme
+  server.auth.strategy('jwt', 'jwt', true,
+  { key: process.env.JWT_SECRET,
+    validateFunc: require('./lib/hapi_auth_jwt2_validate.js'),
+    verifyOptions: { ignoreExpiration: true }
+  });
+
+  server.route([{
+    method: 'GET',
+    path: '/',
+    config: { auth : false },
+    handler: function(request, reply) {
+      var url    = server.generate_google_oauth2_url();
+  		var imgsrc = 'https://developers.google.com/accounts/images/sign-in-with-google.png';
+  		var btn    = '<a href="' + url +'"><img src="' +imgsrc +'" alt="Login With Google"></a>'
+      reply(btn);
+    }
+  },
+  {
+    method: 'GET',
+    path: '/calendar',
+    config: { auth : 'jwt' },
+    handler: function(request, reply) {
+      reply('show calendar here...');
+    }
+  }
+
+  ]);
+
+});
+
+server.start(function(err){ // boots your server
+  assert(!err, "FAILED TO Start Server", err);
+	console.log('Now Visit: http://localhost:'+server.info.port);
+});
+
+module.exports = server;
