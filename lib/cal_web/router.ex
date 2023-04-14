5a4ad59f6bc8c406fbd546dc7efc9e49cb12b6be
Luís Arteiro <lucho14garfield@gmail.com>
2023-04-14 11:44:58 +0100

    feat: Refactoring coveralls.json and commenting unneeded API to get coverage back to 100%. #22

--- lib/cal_web/router.ex
+++ lib/cal_web/router.ex
@@ -10,9 +10,9 @@ defmodule CalWeb.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
+  # pipeline :api do
+  #   plug :accepts, ["json"]
+  # end
 
   scope "/", CalWeb do
     pipe_through :browser
