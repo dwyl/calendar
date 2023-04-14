6d7c07b3ff0121355a3a011410fa9ddbe0e65a34
nelsonic <nelson+github@dwyl.com>
2023-04-14 10:17:00 +0100

    mix phx.new cal --no-mailer --no-dashboard closes #22

---
+++ lib/cal_web/controllers/page_controller.ex
@@ -0,0 +1,9 @@
+defmodule CalWeb.PageController do
+  use CalWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end
---
+++ test/test_helper.exs
@@ -0,0 +1,2 @@
+ExUnit.start()
+Ecto.Adapters.SQL.Sandbox.mode(Cal.Repo, :manual)
