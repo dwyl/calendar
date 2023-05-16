d178ab0b261b2be6aec57cf9a4e93adeff814514
nelsonic <nelson+github@dwyl.com>
2023-05-16 10:38:43 +0100

    dbg conn for #38

--- lib/cal_web/controllers/page_controller.ex
+++ lib/cal_web/controllers/page_controller.ex
@@ -4,7 +4,9 @@ defmodule CalWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
+    dbg(conn)
     oauth_google_url = ElixirAuthGoogle.generate_oauth_url(conn)
+    dbg(oauth_google_url)
     render(conn, :home, layout: false, oauth_google_url: oauth_google_url)
   end
 end
